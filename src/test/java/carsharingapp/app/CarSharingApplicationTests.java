bf9fe8f623ca294825b3217408bf2ef8d1929e34
petro feniak <petro.fenyak@gmail.com>
2025-05-18 21:56:22 +0300

    first commit

---
+++ src/test/java/carsharingapp/app/CarSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package carsharingapp.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSharingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
