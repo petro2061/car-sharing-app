dafd0cfa001a2c7ce80c4af755b9d58313b838b2
petro feniak <petro.fenyak@gmail.com>
2025-05-24 20:06:32 +0300

    fixed code style

--- src/main/java/carsharingapp/app/model/Car.java
+++ src/main/java/carsharingapp/app/model/Car.java
@@ -1,14 +1,20 @@
 package carsharingapp.app.model;
 
 import carsharingapp.app.enums.CarType;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
-
-import java.math.BigDecimal;
 
 @Getter
 @Setter
