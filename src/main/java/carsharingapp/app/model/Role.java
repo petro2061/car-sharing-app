91e4d7b060bef5125edda571726e0edf499b8d86
petro feniak <petro.fenyak@gmail.com>
2025-05-27 21:03:58 +0300

    added new entity class Role

---
+++ src/main/java/carsharingapp/app/model/Role.java
@@ -0,0 +1,29 @@
+package carsharingapp.app.model;
+
+import carsharingapp.app.enums.RoleType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, unique = true)
+    private RoleType roleType;
+}
