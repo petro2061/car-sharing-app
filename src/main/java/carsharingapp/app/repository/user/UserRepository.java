5352cf0aeb93cbb2bf55cd4cbc3c6f7bdaa9905b
petro feniak <petro.fenyak@gmail.com>
2025-10-21 10:29:57 +0300

    added new method findByIdWithRole

--- src/main/java/carsharingapp/app/repository/user/UserRepository.java
+++ src/main/java/carsharingapp/app/repository/user/UserRepository.java
@@ -1,9 +1,10 @@
-package carsharingapp.app.repository;
+package carsharingapp.app.repository.user;
 
 import carsharingapp.app.model.User;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -12,4 +13,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
     boolean existsByEmail(String email);
+
+    @Query("SELECT u FROM User u LEFT JOIN FETCH u.roles WHERE u.id = :id")
+    Optional<User> findByIdWithRole(Long id);
 }
