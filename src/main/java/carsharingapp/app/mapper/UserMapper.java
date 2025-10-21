08deadf47666ed0bb7fc57cdb0245166b9f8dc8a
petro feniak <petro.fenyak@gmail.com>
2025-10-21 10:27:11 +0300

    added new default method mapRoleTypes

--- src/main/java/carsharingapp/app/mapper/UserMapper.java
+++ src/main/java/carsharingapp/app/mapper/UserMapper.java
@@ -3,13 +3,18 @@ package carsharingapp.app.mapper;
 import carsharingapp.app.config.MapperConfig;
 import carsharingapp.app.dto.user.UserRequestDto;
 import carsharingapp.app.dto.user.UserResponseDto;
+import carsharingapp.app.enums.RoleType;
+import carsharingapp.app.model.Role;
 import carsharingapp.app.model.User;
+import java.util.Set;
+import java.util.stream.Collectors;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
-@Mapper(config = MapperConfig.class)
+@Mapper(config = MapperConfig.class, uses = User.class)
 public interface UserMapper {
-    @Mapping(target = "role", ignore = true)
+    @Mapping(target = "roles", source = "user", qualifiedByName = "mapRoleTypes")
     UserResponseDto toResponseDto(User user);
 
     @Mapping(target = "id", ignore = true)
@@ -17,4 +22,14 @@ public interface UserMapper {
     @Mapping(target = "deleted", ignore = true)
     @Mapping(target = "authorities", ignore = true)
     User toUserModel(UserRequestDto userRequestDto);
+
+    @Named("mapRoleTypes")
+    default Set<RoleType> mapRoleTypes(User user) {
+        if (user.getRoles() == null) {
+            return Set.of();
+        }
+        return user.getRoles().stream()
+                .map(Role::getRoleType)
+                .collect(Collectors.toSet());
+    }
 }
